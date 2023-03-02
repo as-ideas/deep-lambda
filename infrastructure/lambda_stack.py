8ab3d2e587780eafc164f2d619299846f095c68b
cschafer26 <c.schaefer.home@gmail.com>
2023-03-02 12:06:16 +0100

    Add PYTHONPATH env.

--- infrastructure/lambda_stack.py
+++ infrastructure/lambda_stack.py
@@ -1,6 +1,5 @@
 from aws_cdk import (
     aws_apigateway as api,
-    aws_ecs as ecs,
     aws_s3 as s3,
     aws_ec2 as ec2,
     aws_ecr as ecr,
@@ -26,11 +25,6 @@ class LambdaStack(core.Stack):
         self.model_bucket = s3.Bucket.from_bucket_name(scope=self,
                                                        id=f'{id}-model-bucket',
                                                        bucket_name='deep-lambda')
-        self.ecs_cluster = ecs.Cluster(scope=self,
-                                       id=f'{id}-ecs',
-                                       cluster_name='deep-lambda-serving-ecs',
-                                       vpc=self.vpc,
-                                       container_insights=True)
 
         self.ecr_repository = ecr.Repository.from_repository_name(scope=self, id='deep-lambda-ecr-repo',
                                                                   repository_name=f'deep-lambda')
@@ -41,8 +35,9 @@ class LambdaStack(core.Stack):
             vpc=self.vpc,
             runtime=aws_lambda.Runtime.FROM_IMAGE,
             ephemeral_storage_size=Size.gibibytes(2),
-            code=aws_lambda.Code.from_ecr_image(repository=self.ecr_repository),
+            code=aws_lambda.EcrImageCode(self.ecr_repository),
             handler=aws_lambda.Handler.FROM_IMAGE,
+            memory_size=8384
         )
 
         self.model_bucket.grant_read(self.lambda_function)
